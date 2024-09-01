c3add952130ac5e5abd68d9011afa0d66ddc2f53
María <146212302+mariasoledadperozo@users.noreply.github.com>
2024-09-01 16:51:13 +0200

    Perplexity

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-var resultado = document.getElementById('resultado').value; 
-var boton = document.getElementById('button').value; 
+var resultado = document.getElementById('resultado');
 
-boton.onclick = function(value){
-    resultado.value += this.value; 
+
+function colocarNumero(numero){
+    resultado.value += numero; 
+}
+
+
+function borrarNumero(){
+    resultado.value='  '; 
 }
